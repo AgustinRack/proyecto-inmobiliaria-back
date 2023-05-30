d81fd3451e3e63279e3ac82e0989e1188c284c11
Tu Nombre <agustinrack14@hotmail.com>
2023-05-30 17:25:15 -0300

    models favorites

--- models/Properties.js
+++ models/Properties.js
@@ -1,24 +1,22 @@
-const Sequelize = require ("sequelize")
-const db = require ("../db")
-const bcrypt = require ("bcrypt")
+const Sequelize = require("sequelize");
+const db = require("../db");
+const bcrypt = require("bcrypt");
 
+class Properties extends Sequelize {}
 
-class Properties extends Sequelize {
+Properties.init(
+  {
+    type: { type: Sequelize.INTEGER, allowNull: false },
+    price: { type: Sequelize.INTEGER, allowNull: false },
+    country: { type: Sequelize.STRING, allowNull: false },
+    neighborhood: { type: Sequelize.STRING, allowNull: false },
+    address: { type: Sequelize.STRING, allowNull: false },
+    size: { type: Sequelize.INTEGER, allowNull: false },
+    bedrooms: { type: Sequelize.INTEGER, allowNull: false },
+    bathrooms: { type: Sequelize.INTEGER, allowNull: false },
+    description: { type: Sequelize.STRING, allowNull: false },
+  },
+  { sequelize: db, modelName: "properties" }
+);
 
-}
-
-Properties.init ({
-    
-    type: {type: Sequelize.INTEGER, allowNull: false},
-    price: {type: Sequelize.INTEGER, allowNull: false},
-    country: {type: Sequelize.STRING, allowNull: false},
-    neighborhood: {type: Sequelize.STRING, allowNull: false},
-    address: {type: Sequelize.STRING, allowNull: false},
-    size: {type: Sequelize.INTEGER, allowNull: false},
-    bedrooms: {type: Sequelize.INTEGER, allowNull: false},
-    bathrooms: {type: Sequelize.INTEGER, allowNull: false},
-    description: {type: Sequelize.STRING, allowNull: false}
-}, {sequelize: db, modelName: "properties"}) 
-
-
-module.exports = Properties
+module.exports = Properties;
