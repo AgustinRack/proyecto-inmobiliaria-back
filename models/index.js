d8d0ee1cb2d9210931675cb4d1a5af2a40b9e21f
Gonzalo Molina <molinagomezgonzalo@gmail.com>
2023-05-31 08:56:54 -0300

    Table relationship

--- models/index.js
+++ models/index.js
@@ -4,4 +4,19 @@ const Properties = require("./Properties");
 const Categories = require("./Categories");
 const Favorites = require("./Favorites");
 
+Users.hasMany(Favorites);
+Favorites.belongsTo(Users);
+
+Users.hasMany(Visits);
+Visits.belongsTo(Users);
+
+Properties.hasMany(Visits);
+Visits.belongsTo(Properties);
+
+Properties.hasMany(Favorites);
+Favorites.belongsTo(Properties);
+
+Categories.hasMany(Properties);
+Properties.belongsTo(Categories);
+
 module.exports = { Users, Visits, Properties, Categories, Favorites };
