f7372fbdcc52c400cf0d12a21e0ad0804e3cdeaa
Tu Nombre <agustinrack14@hotmail.com>
2023-06-06 16:14:43 -0300

    delete-mod-property

--- controller/propertiesController.js
+++ controller/propertiesController.js
@@ -7,4 +7,50 @@ const allProperties = async (req, res) => {
   });
 };
 
-module.exports = { allProperties };
+const deleteProperty = async (req, res) => {
+  const { id } = req.params;
+
+  Properties.destroy({ where: { id } })
+    .then(() => {
+      res.status(202).send("Producto eliminado");
+    })
+    .catch((error) => console.log(error));
+};
+
+const modProperty = async (req, res) => {
+  const { id } = req.params;
+  const {
+    size,
+    bathrooms,
+    bedrooms,
+    country,
+    neighborhood,
+    address,
+    description,
+    price,
+    img,
+    imgs,
+    categoryId,
+  } = req.body;
+
+  Properties.update(
+    {
+      price: price,
+      country: country,
+      neighborhood: neighborhood,
+      address: address,
+      size: size,
+      bedrooms: bedrooms,
+      bathrooms: bathrooms,
+      description: description,
+      img: img,
+      imgs: imgs,
+      categoryId: categoryId,
+    },
+    { where: { id } }
+  )
+    .then((properties) => res.status(200).send(properties))
+    .catch((error) => console.log(error));
+};
+
+module.exports = { allProperties, deleteProperty, modProperty };
