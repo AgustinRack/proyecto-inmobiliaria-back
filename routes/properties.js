89f4e4f7dc2675cc443c8407b23d77b5b4033533
Tu Nombre <agustinrack14@hotmail.com>
2023-06-07 12:25:02 -0300

    admin-routes

--- routes/properties.js
+++ routes/properties.js
@@ -4,8 +4,6 @@ const { Properties } = require("../models");
 const { propertiesController } = require("../controller");
 
 router.get("/all", propertiesController.allProperties);
-router.delete("/admin/:id", propertiesController.deleteProperty);
-router.put("/mod/:id", propertiesController.modProperty);
 
 router.get("/for-rent", propertiesController.getPropertiesForRent);
 router.get("/for-sale", propertiesController.getPropertiesForSale);
