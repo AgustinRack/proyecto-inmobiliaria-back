da91180722b01e19eae5d30e148f46f5ab57db69
Tu Nombre <agustinrack14@hotmail.com>
2023-06-09 14:14:01 -0300

    edit-province

--- models/Properties.js
+++ models/Properties.js
@@ -8,6 +8,7 @@ Properties.init(
     is_for_rent: { type: Sequelize.BOOLEAN, allowNull: false },
     price: { type: Sequelize.INTEGER, allowNull: false },
     country: { type: Sequelize.STRING, allowNull: false },
+    province: { type: Sequelize.STRING, allowNull: false },
     neighborhood: { type: Sequelize.STRING, allowNull: false },
     address: { type: Sequelize.STRING, allowNull: false },
     size: { type: Sequelize.INTEGER, allowNull: false },
