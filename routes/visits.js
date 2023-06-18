9978ca4e1bca175ba68d6b45d72ae9cc1bc34519
MolinaG96 <molinagomezgonzalo@gmail.com>
2023-06-18 20:16:54 -0300

    User visit route create

--- routes/visits.js
+++ routes/visits.js
@@ -4,5 +4,6 @@ const { visitsController } = require("../controller");
 
 router.post("/appointment/save", visitsController.saveVisit);
 router.get("/appointment/:id", visitsController.getPropertyVisits);
+router.get("/appointment/user/:id", visitsController.getUserVisits);
 
 module.exports = router;
